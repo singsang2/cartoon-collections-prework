3753f1ec8843fc27e90c665fa385cd19d9e2198d
Sung Bae <singsang2@gmail.com>
2018-10-06 21:31:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,8 @@ def long_planeteer_calls(list)
   list.any? {|caller| caller.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(list)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each {|cheese| true if list.include?(cheese)}
 end
